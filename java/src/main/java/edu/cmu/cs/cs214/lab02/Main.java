9e58b7e9b80e0646079a42360bb4879a3ae391b9
JFL421 <fuyangl@andrew.cmu.edu>
2024-01-24 13:28:02 -0500

    First commit for lab2: information hiding and encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,28 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+// import edu.cmu.cs.cs214.lab02.shapes.Circle;
+// import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape shape = new Rectangle(2, 3);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer = new Renderer(shape);
 
         renderer.draw();
+
+        // Shape shape1 = new Circle(2);
+
+        // Renderer renderer1 = new Renderer(shape1);
+
+        // renderer1.draw();
+
+        // Shape shape2 = new Square(3);
+
+        // Renderer renderer2 = new Renderer(shape2);
+
+        // renderer2.draw();
     }
 }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -2,15 +2,30 @@ package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
-public class Renderer {
-    public Rectangle rectangle;
+// public class Renderer {
+//     public Rectangle rectangle;
     
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+//     Renderer(Rectangle rectangle) {
+//         this.rectangle = rectangle;
+//     }
+
+//     void draw() {
+//         double area = rectangle.getArea();
+
+//         // assume implementation
+
+//         System.out.println("Shape printed\n" + "Its area is " + area);
+//     }
+// }
+public class Renderer {
+    public Shape shape;
+    
+    Renderer(Shape shape) {
+        this.shape = shape;
     }
 
     void draw() {
-        double area = rectangle.getArea();
+        double area = shape.getArea();
 
         // assume implementation
 
